37a79f1d0da5132f420f0d4b6b40fb5243a66735
Administrator <863655243@qq.com>
2022-06-23 16:33:42 +0800

    我想回家！

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="我想回家！"/>
   </div>
 </template>
 
